87cefe3bca12695cdee1d0139b2cfbb28afb08fb
Maxime Bret <maxime.bret.ext@boursorama.fr>
2016-10-12 19:26:15 +0200

    no message

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var config = require("./config");
-
+// Express body-parser (only for post/put/... raw content)
+var getRawBody  = require('raw-body');
 var app = express();
 
 // view engine setup
@@ -14,8 +15,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/js", express.static(path.join(__dirname, '../node_modules')));
@@ -23,6 +26,7 @@ app.use("/js", express.static(path.join(__dirname, '../node_modules')));
 app.use('/', routes);
 
 app.locals.config = config;
+req.app.locals.sleepMode = false;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,12 @@ router.get('/', function(req, res) {
 });
 
 router.post("/ring", function(req, res) {
+    if (req.app.locals.sleepMode) {
+        console.info("sleep mode enabled, ignore ring");
+        return res.status(400).json({
+            code: "sleepMode"
+        });
+    }
     if (req.app.locals.socketRingClient) {
         console.info("send ring event");
         req.app.locals.socketRingClient.emit("ring");
@@ -24,6 +30,20 @@ router.get("/client", function(req, res) {
     }
 
     return res.status(404).json({});
+});
+
+router.put("/state", function(req, res) {
+    var sleep = req.body.sleep ? req.body.sleep === "true" : undefined;
+
+    if (sleep === true) {
+        console.info("enter sleep mode");
+        req.app.locals.sleepMode = true;
+    } else if (sleep === false) {
+        console.info("leave sleep mode");
+        req.app.locals.sleepMode = false;
+    }
+
+    return res.status(200).json();
 });
 
 module.exports = router;
